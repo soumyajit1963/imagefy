2ed837373ac517ad46044cdca46881bb3835fd92
das_demo <sooumyajit1903@gmail.com>
2025-03-17 11:37:57 +0530

    first commit

---
+++ client/src/components/Description.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Description = () => {
+  return (
+    <div className='flex flex-col items-center justify-center my-24 p-6 md:px-28'>
+      <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Create AI Images</h1>
+      <p className='text-gray-500 mb-8'>Turn your imagination into visuals</p>
+      <div className='flex flex-col gap-5 md:gap-14 md:flex-row items-center '> 
+        <img src={assets.sample_img_1} alt="" className='w-80 cl:w-96 rounded-lg'/>
+        <div>
+          <h2 className='text-3xl font-medium max-w-lg mb-4'>Introducing the AI-Powered Text to Image Generator </h2>
+          <p className='text-gray-600'>Transform your words into stunning visuals with our AI-powered Text-to-Image Generator! Simply enter a description, and our advanced AI will create unique, high-quality images based on your input. Whether you're an artist, designer, or just exploring creative ideas, bring your imagination to life effortlessly.</p>
+          
+          <p className='text-gray-600'>Simplify type in a text prompt,and our cutting-edge AI will generate high-Quality images in seconds.From product visuals to character designs and portraits, even conceptss that don't yet exits can be visualized effortlessly. Powered by advanced AI technology , the creative possibilities are limitless! </p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Description
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,58 @@
+import React, {  useContext, useEffect, useState } from 'react'
+import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
+
+const Login = () => {
+
+const [state,setState] = useState('Login')
+const {setShowLogin} = useContext(AppContext)
+
+
+
+useEffect(()=>{
+     document.body.style.overflow = 'hidden';
+      return ()=>{
+        document.body.style.overflow = 'unset';
+      }
+},[])
+
+  return (
+    <div className='fixed inset-0 z-50 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
+
+      <form className='relative bg-white p-10 rounded-xl text-slate-500'>
+        <h1 className='text-center text-2xl text-neutral-700 font-medium' >{state}</h1>
+        <p>Welcome back! Please sign in to continue</p>
+
+       { state !== 'Login' && <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
+          <img src={assets.user_icon} alt="" />
+          <input type="text" className='outline-none text-sm' placeholder='Full Name' required />
+        </div>}
+
+        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
+          <img src={assets.email_icon} alt="" />
+          <input type="email" className='outline-none text-sm' placeholder='Email id' required />
+        </div>
+
+        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
+          <img src={assets.lock_icon} alt="" />
+          <input type="password" className='outline-none text-sm' placeholder='Password' required />
+        </div>
+
+        <p className='text-sm text-blue-600 my-4 cursor-pointer'>Forgot password?</p>
+        <button className='bg-blue-600 w-full text-white py-2 rounded-full'>{state === 'Login'?'login':'create account'}</button>
+
+        { state === 'Login' ? <p className='mt-5 text-center'>Don't have na account?<span className='text-blue-600 cursor-pointer' onClick={()=>setState('Sign Up')}>Sign Up</span></p>
+        :
+
+        <p className='mt-5 text-center'>Already have na account?<span className='text-blue-600 cursor-pointer' onClick={()=>setState('Login')}>Login</span></p>}
+    
+
+        <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt='' className='absolute top-5 right-5 cursor-pointer '/>
+
+      </form>
+
+    </div>
+  )
+}
+
+export default Login
